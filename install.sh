80906f38d0e0336383e0889651ff221cc5d1746a
Stephen Thirlwall <stephent@strategicdata.com.au>
2011-02-24 09:42:56 +1100

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+do_install() {
+    local basefile=$1
+    local rccmd=$2
+
+    [ -z $rccmd ] && rccmd="source ~/.dotfiles/$basefile"
+
+    local sysfile=~/.${basefile}
+    local dotfile="~/.dotfiles/${basefile}"
+
+    echo -n "Installing ${dotfile} to ${sysfile} ... "
+    if [ -e $sysfile ] && grep -F -q $dotfile $sysfile; then
+        echo already installed.
+    else
+        echo ${rccmd} >> ${sysfile}
+        echo ok.
+    fi
+}
+
+do_install bashrc
+do_install vimrc
+do_install gvimrc
