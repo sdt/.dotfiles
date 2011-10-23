067c836d2a0259560df40b026044d9c69f514e29
Stephen Thirlwall <sdt@dr.com>
2011-10-23 14:37:52 +1100

    tmux color setup now only local

--- tmux_colors.sh
+++ tmux_colors.sh
@@ -4,31 +4,31 @@
 case $SOLARIZED in
 
     dark)
-        tmux set -g window-status-current-attr dim
-        tmux set -g window-status-current-bg black      # base02
-        tmux set -g window-status-current-fg yellow     # yellow
+        tmux set window-status-current-attr dim
+        tmux set window-status-current-bg black      # base02
+        tmux set window-status-current-fg yellow     # yellow
 
-        tmux set -g status-attr bold
-        tmux set -g status-bg black                     # base02
-        tmux set -g status-fg green                     # base01
+        tmux set status-attr bold
+        tmux set status-bg black                     # base02
+        tmux set status-fg green                     # base01
         ;;
 
     light)
-        tmux set -g window-status-current-attr dim
-        tmux set -g window-status-current-bg white      # base2
-        tmux set -g window-status-current-fg yellow     # yellow
+        tmux set window-status-current-attr dim
+        tmux set window-status-current-bg white      # base2
+        tmux set window-status-current-fg yellow     # yellow
 
-        tmux set -g status-attr bold
-        tmux set -g status-bg white                     # base2
-        tmux set -g status-fg cyan                      # base1
+        tmux set status-attr bold
+        tmux set status-bg white                     # base2
+        tmux set status-fg cyan                      # base1
         ;;
 
     *)
-        tmux set -g status-bg blue
-        tmux set -g status-fg white
-        tmux set -g status-right-fg yellow
-        tmux set -g window-status-current-bg cyan
-        tmux set -g window-status-current-fg black
+        tmux set status-bg blue
+        tmux set status-fg white
+        tmux set status-right-fg yellow
+        tmux set window-status-current-bg cyan
+        tmux set window-status-current-fg black
         ;;
 
 esac
