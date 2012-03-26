4b35c038a6f086b09a287bb3cf66b01a23dba638
Stephen Thirlwall <sdt@dr.com>
2012-03-26 21:42:49 +1100

    Simplify tmux color specs

--- tmux_colors.sh
+++ tmux_colors.sh
@@ -1,38 +1,20 @@
-# Color component of foreground colour+attr pair
-export SOLARIZED_BASE03_FG=black
-export SOLARIZED_BASE02_FG=black
-export SOLARIZED_BASE01_FG=green
-export SOLARIZED_BASE00_FG=yellow
-export SOLARIZED_BASE0_FG=blue
-export SOLARIZED_BASE1_FG=cyan
-export SOLARIZED_BASE2_FG=white
-export SOLARIZED_BASE3_FG=white
-export SOLARIZED_YELLOW_FG=yellow
-export SOLARIZED_ORANGE_FG=red
-export SOLARIZED_RED_FG=red
-export SOLARIZED_MAGENTA_FG=magenta
-export SOLARIZED_VIOLET_FG=magenta
-export SOLARIZED_BLUE_FG=blue
-export SOLARIZED_CYAN_FG=cyan
-export SOLARIZED_GREEN_FG=green
-
-# Attr component of foreground color+attr pair
-export SOLARIZED_BASE03_ATTR=bright
-export SOLARIZED_BASE02_ATTR=dim
-export SOLARIZED_BASE01_ATTR=bright
-export SOLARIZED_BASE00_ATTR=bright
-export SOLARIZED_BASE0_ATTR=bright
-export SOLARIZED_BASE1_ATTR=bright
-export SOLARIZED_BASE2_ATTR=dim
-export SOLARIZED_BASE3_ATTR=bright
-export SOLARIZED_YELLOW_ATTR=dim
-export SOLARIZED_ORANGE_ATTR=bright
-export SOLARIZED_RED_ATTR=dim
-export SOLARIZED_MAGENTA_ATTR=dim
-export SOLARIZED_VIOLET_ATTR=bright
-export SOLARIZED_BLUE_ATTR=dim
-export SOLARIZED_CYAN_ATTR=dim
-export SOLARIZED_GREEN_ATTR=dim
+# Foreground colour,attr pair
+export SOLARIZED_BASE03_FG=black,bright
+export SOLARIZED_BASE02_FG=black,dim
+export SOLARIZED_BASE01_FG=green,bright
+export SOLARIZED_BASE00_FG=yellow,bright
+export SOLARIZED_BASE0_FG=blue,bright
+export SOLARIZED_BASE1_FG=cyan,bright
+export SOLARIZED_BASE2_FG=white,dim
+export SOLARIZED_BASE3_FG=white,bright
+export SOLARIZED_YELLOW_FG=yellow,dim
+export SOLARIZED_ORANGE_FG=red,bright
+export SOLARIZED_RED_FG=red,dim
+export SOLARIZED_MAGENTA_FG=magenta,dim
+export SOLARIZED_VIOLET_FG=magenta,bright
+export SOLARIZED_BLUE_FG=blue,dim
+export SOLARIZED_CYAN_FG=cyan,dim
+export SOLARIZED_GREEN_FG=green,dim
 
 # Background colors don't have attributes in curses (do they?)
 export SOLARIZED_BASE02_BG=black
@@ -49,9 +31,12 @@ set_tmux_color() {
     local fg_color=$2
     local bg_color=$3
 
+    # 1st: ${pair/%,*/}
+    # 2nd: ${pair/#*,/}
+
     eval "export TMUX_COLOR_${var_base}_BG=\$SOLARIZED_${bg_color}_BG"
-    eval "export TMUX_COLOR_${var_base}_FG=\$SOLARIZED_${fg_color}_FG"
-    eval "export TMUX_COLOR_${var_base}_ATTR=\$SOLARIZED_${fg_color}_ATTR"
+    eval "export TMUX_COLOR_${var_base}_FG=\${SOLARIZED_${fg_color}_FG/%,*/}"
+    eval "export TMUX_COLOR_${var_base}_ATTR=\${SOLARIZED_${fg_color}_FG/#*,/}"
 }
 
 # setup tmux colors
@@ -62,6 +47,7 @@ case $SOLARIZED in
         set_tmux_color WINDOW_STATUS_CURRENT    YELLOW  BASE02
         ;;
 
+    # This is unused now (we have an inverted dark scheme for light)
     light)
         set_tmux_color STATUS                   BASE1   BASE2
         set_tmux_color WINDOW_STATUS_CURRENT    YELLOW  BASE2
