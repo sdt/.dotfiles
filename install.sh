16d14168ba95811a50b7ed6d74bec33ddf4fd38d
Stephen Thirlwall <sdt@dr.com>
2015-03-02 16:46:20 +1100

    Sort the git config options

--- install.sh
+++ install.sh
@@ -39,22 +39,22 @@ do_install sqliterc ".read $HOME/.dotfiles/sqliterc"
 install_link ~/.dotfiles/colordiffrc ~/.colordiffrc
 
 echo -n "Updating git config ... "
-git config --global core.excludesfile ~/.dotfiles/gitignore
-git config --global color.ui true
-git config --global alias.llog 'log --date=local'
-git config --global alias.ds 'diff --stat'
+git config --global alias.blog '!sh -c "git lg $( git merge-base $1 master )^..$1" -'
+git config --global alias.cf 'commit -m FIXME'
+git config --global alias.chp 'cherry-pick -n -x'
+git config --global alias.co 'checkout'
+git config --global alias.cv 'commit -v'
 git config --global alias.dc 'diff --cached'
+git config --global alias.ds 'diff --stat'
 git config --global alias.dsc 'diff --stat --cached'
-git config --global alias.steve 'tag -f steve-was-here'
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+git config --global alias.llog 'log --date=local'
+git config --global alias.recent-branches "for-each-ref --sort=-committerdate refs/heads/ --format='%(committerdate:short) %(authorname) %(refname:short)'"
+git config --global alias.st 'status -s'
+git config --global alias.steve 'tag -f steve-was-here'
+git config --global alias.w 'whatchanged -M -C -B'
 git config --global alias.wu 'log --stat origin..@{0}'
 git config --global alias.wup 'log -p origin..@{0} --'
-git config --global alias.w 'whatchanged -M -C -B'
-git config --global alias.cv 'commit -v'
-git config --global alias.cf 'commit -m FIXME'
-git config --global alias.co 'checkout'
-git config --global alias.st 'status -s'
-git config --global alias.chp 'cherry-pick -n -x'
-git config --global alias.recent-branches "for-each-ref --sort=-committerdate refs/heads/ --format='%(committerdate:short) %(authorname) %(refname:short)'"
-git config --global alias.blog '!sh -c "git lg $( git merge-base $1 master )^..$1" -'
+git config --global color.ui true
+git config --global core.excludesfile ~/.dotfiles/gitignore
 echo ok.
